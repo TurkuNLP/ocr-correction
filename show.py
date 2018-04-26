a48e34deee2372b524b72991a96892d4a96da4af
Kai Hakala <kahaka@utu.fi>
2018-04-26 15:46:16 +0300

    Script for reading OpenNMT predictions

---
+++ show.py
@@ -0,0 +1,36 @@
+"""
+Helper to read OpenNMT predictions
+"""
+
+import argparse
+import itertools
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input')
+parser.add_argument('predictions')
+parser.add_argument('gold', nargs='?')
+
+args = parser.parse_args()
+
+def opennmt2txt(data):
+    """
+    Converts OpenNMT output to a readable format
+    """
+    res = []
+    for line in data:
+        clean_line = line.replace('BEG', '').replace(' ', '').replace('_', '')
+        res.append(clean_line)
+    return res
+
+input_data = open(args.input)
+pred_data = open(args.predictions)
+if args.gold:
+    gold_data = open(args.gold)
+else:
+    gold_data = []
+    
+s = [opennmt2txt(data) for data in [input_data, pred_data, gold_data]]
+
+for i, p, g in itertools.zip_longest(*s):
+    print(''.join([i, p, str(g)]))
+    print('\n')
