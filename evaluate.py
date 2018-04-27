42eb628dc487ee72c19ef3d3435dab080b4009ac
Kai Hakala <kahaka@utu.fi>
2018-04-27 05:22:10 +0300

    Truncation option for evaluation

--- evaluate.py
+++ evaluate.py
@@ -4,11 +4,12 @@ from sklearn.metrics import accuracy_score
 
 ''' Evaluate results, just accuracy for now '''
 
-def evaluate(pred_f, gold_f, eval_level):
+def evaluate(pred_f, gold_f, eval_level, truncate):
 	predictions = open(pred_f, "r").readlines()
 	gold = open(gold_f, "r").readlines()
 
 	p, t = [], []
+
 	for i in range(len(predictions)):
 			pred_seq, gold_seq = predictions[i].strip().split(" "), gold[i].strip().split(" ")
 
@@ -18,9 +19,14 @@ def evaluate(pred_f, gold_f, eval_level):
 			else:
 				## if pred is messed up, it will be longer/shorter
 				# Now we penalize the predictions if they are longer than the gold sequence
-				for pred_token, gold_token in zip_longest(pred_seq, gold_seq, fillvalue=''):
-					p.append(pred_token)
-					t.append(gold_token)
+				if truncate:
+					for i in range(len(gold_seq)):
+						p.append(pred_seq[i] if len(pred_seq) > i else '')
+						t.append(gold_seq[i])						
+				else:
+					for pred_token, gold_token in zip_longest(pred_seq, gold_seq, fillvalue=''):
+						p.append(pred_token)
+						t.append(gold_token)
 					
 	acc = accuracy_score(p, t)
 	print("Prediction accuracy: {}".format(acc))
@@ -32,8 +38,9 @@ if __name__ == "__main__":
 	parser.add_argument("--pred", required=True)
 	parser.add_argument("--gold", required=True)
 	parser.add_argument("--eval_level", help="char or word, default=word", default="word")
+	parser.add_argument("--truncate", help="Truncate predictions to gold length", action="store_true")
 
 	args = parser.parse_args()
 	print(args)
 
-	evaluate(args.pred, args.gold, args.eval_level)
+	evaluate(args.pred, args.gold, args.eval_level, args.truncate)
