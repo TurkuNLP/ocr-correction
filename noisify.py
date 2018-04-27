2ab21943422937fdf44332bcd1b24fe303c51e9f
Aleksi <avjves@utu.fi>
2018-04-27 15:42:28 +0300

    Change data formats

--- noisify.py
+++ noisify.py
@@ -9,7 +9,7 @@ def make_distribution(loc):
 	dist = json.load(open(loc, "r"))
 	distribution = {}
 	for key, replacement_dict in dist.items():
-		#print(key)
+
 		if key in replacement_dict:
 			del replacement_dict[key]
 		new_values = []
@@ -18,48 +18,62 @@ def make_distribution(loc):
 
 		for replacement_key, replacement_value in replacement_dict.items():
 			new_values.append([replacement_value / replacement_sum, replacement_key])
-		#float + +  doesn't necessarily add to 1.0 (0.9999999..), so
 		new_values.sort(key=itemgetter(0))
 		values, keys = [v[0] for v in new_values], [v[1] for v in new_values]
 		if len(new_values) != 0:
 			distribution[key] = [values, keys]
 
-		#print(distribution[key])
-	#	input()
-
 	return distribution
 
-def noisify(input_loc, distribution, noise_level):
-	data = open(input_loc, "r").readlines()
-	noisified_sentences = []
-	for sent in data:
-		sent = sent.strip()
-		if not sent: continue
-		print(sent)
-		sent = list(sent)
-		sent_indexes = list(range(len(sent)))
-		char_indexes = np.random.choice(sent_indexes, int(len(sent_indexes) * float(noise_level)))
+def noisify(input_loc, output_loc, distribution, noise_level):
+	data = json.load(gzip.open(input_loc, "rt"))
+	documents = []
+	for document_id, document_text in data.items():
+		if not document_text: continue
+
+		doc_list = [document_id]
+		document_text = " ".join(document_text.split()) ##remove new line etc.
+
+		orig_text = document_text
+		text = list(document_text)
+
+		text_indexes = list(range(len(text)))
+		char_indexes = np.random.choice(text_indexes, int(len(text_indexes) * float(noise_level)))
+
 		for char_index in char_indexes:
-			char = sent[char_index]
+			char = text[char_index]
 			if char in distribution:
 				char_distribution_values, char_distribution_keys = distribution[char]
 				new_char = np.random.choice(char_distribution_keys, p=char_distribution_values)
+				if new_char in [" ", "\t", "\n"]:
+					new_char = char
 			else:
 				new_char = char
-			sent[char_index] = new_char
-		noisified_sentences.append("".join(sent))
-		print("".join(sent))
-		print()
-	return noisified_sentences
+			text[char_index] = new_char
+
+		text = "".join(text)
+		orig_words = orig_text.split(" ")
+		noise_words = text.split(" ")
+		assert len(orig_words) == len(noise_words)
+
+		word_pairs = []
+		for i in range(0, len(orig_words)):
+			word_pairs.append([orig_words[i], noise_words[i]])
+			
+		doc_list.append(word_pairs)
+		documents.append(doc_list)
+
+	gzip.open(output_loc, "wt").write(json.dumps(documents))
 
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser("Generate noisy data. Input needs to be sentence per line.")
-	parser.add_argument("--input", help="Input file. One sentence per line.", required=True)
+	parser = argparse.ArgumentParser("Generate noisy data. Input needs to be JSON-format, key = doc_id, value = text")
+	parser.add_argument("--input", help="Input file.", required=True)
+	parser.add_argument("--output", help="Output file.", required=True)
 	parser.add_argument("--distribution", help="Character distribution file. JSON-format, key per character, value = dictionary of replacement_key:count values", required=True)
 	parser.add_argument("--noise_level", help="Amount of noise to generate, default=0.1", default=0.1, type=float)
 	args = parser.parse_args()
 	print(args)
 	distribution = make_distribution(args.distribution)
-	noisify(args.input, distribution, args.noise_level)
+	noisify(args.input, args.output, distribution, args.noise_level)
